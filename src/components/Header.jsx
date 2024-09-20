e9effca2d139e6517c0d767713e6c588d2e3fc30
steiner112 <asvmusic26@gmail.com>
2024-09-20 18:10:25 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../components/Header.css';
+import hospitalGif from '../assets/hospital.gif'; // Correctly import the gif
 
-export default function header() {
+export default function Header() { // Capitalized the component name
   return (
-    <>
     <header>
-     <nav>
-     <span id='img-text'> <img src="\src\assets\hospital.svg" alt="logo" /> <h1>Healthcare <span>Services</span></h1></span>  
-     </nav>
-     </header>
-    </>
-  )
+      <nav>
+        <span id="img-text">
+          <img src={hospitalGif} alt="logo" />
+          <h1>Healthcare <span>Services</span></h1>
+        </span>
+      </nav>
+    </header>
+  );
 }
